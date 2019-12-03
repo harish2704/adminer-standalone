59091f5c4d24653613d4c51b0a82b375b31d8400
Harish.K <harish2704@gmail.com>
2019-12-03 13:33:13 +0530

    Fix session path

--- adminer.sh
+++ adminer.sh
@@ -6,4 +6,5 @@ cd "$appDir/www";
 port="$PORT";
 if [ -z "$port" ]; then port="7777"; fi
 
-php -S "0.0.0.0:$port" -d session.save_path=$PWD/sess & echo "Adminer server started.. PID: $!";
+mkdir -p ../sess
+php -S "0.0.0.0:$port" -d session.save_path=$PWD/../sess & echo "Adminer server started.. PID: $!";
