357478492ada2d21c72339a8f1e6c23478c74362
Harish.K <harish2704@gmail.com>
2019-12-09 11:25:19 +0530

    Sqlite3 login without password.

--- install.sh
+++ install.sh
@@ -10,3 +10,9 @@ mv adminer-4.7.5 www
 cat<<EOF > www/index.php
 <?php header("Location: adminer/plugin.php"); ?>
 EOF
+
+# Install & setup FCSqliteConnectionWithoutCredentials plugin.
+cd www/plugins
+wget 'https://raw.githubusercontent.com/FrancoisCapon/LoginToASqlite3DatabaseWithoutCredentialsWithAdminer/master/fc-sqlite-connection-without-credentials.php'
+sed -i '/\$plugins = array(/a \\t\tnew FCSqliteConnectionWithoutCredentials,' ../adminer/plugin.php
+
