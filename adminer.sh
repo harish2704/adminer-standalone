4941c5883995051c4f875c817a671b4a9a1d982f
Harish Karumuthil <harish2704@gmail.com>
2022-08-04 17:05:26 +0530

    Added systemctl service file

--- adminer.sh
+++ adminer.sh
@@ -7,4 +7,9 @@ port="$PORT";
 if [ -z "$port" ]; then port="7777"; fi
 
 mkdir -p ../sess
-php -S "0.0.0.0:$port" -d session.save_path=$PWD/../sess & echo "Adminer server started.. PID: $!";
+cmd="php -S 0.0.0.0:$port -d max_input_vars=2000 -d upload_max_filesize=100M -d post_max_size=100M -d session.save_path=$PWD/../sess"
+if [[ "$1" == '-f' ]]; then
+  exec $cmd
+else
+  exec $cmd & echo "Adminer server started.. PID: $!";
+fi
